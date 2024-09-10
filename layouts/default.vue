c68773f099a38951581f7a7a8d3d47eba8842657
田中拓磨 <tanakatakuma@MacBook.local>
2024-09-10 20:13:06 +0900

    作成

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Header class="header"/>
+    <slot />
+    <Footer />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .header {
+    position: fixed;
+    z-index: 1000;
+  }
+</style>
